37394ef191964fcf328b314589c0c67bee7c8895
Qin Jiangbo <QinJiangbo@users.noreply.github.com>
2017-11-27 17:09:43 +0800

    添加ARFF文件处理工具类

---
+++ src/main/java/com/qinjiangbo/util/ArffCreatorUtil.java
@@ -0,0 +1,20 @@
+package com.qinjiangbo.util;
+
+import weka.core.Instances;
+
+/**
+ * @date: 27/11/2017 5:06 PM
+ * @author: qinjiangbo@github.io
+ * @description:
+ */
+public class ArffCreatorUtil {
+
+    /**
+     * 将数据集保存为Arff文件
+     * @param dataSet
+     * @param arffName
+     */
+    public void saveDataToArff(Instances dataSet, String arffName) {
+
+    }
+}
