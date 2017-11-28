8c37ea29944da298558bac77a3bfa02cb5d5fc05
Qin Jiangbo <QinJiangbo@users.noreply.github.com>
2017-11-28 10:51:32 +0800

    重构聚类分析的代码，整理了数据集所在的路径

---
+++ src/main/java/com/qinjiangbo/algorithms/kmeans/KmeansCluster.java
@@ -0,0 +1,70 @@
+package com.qinjiangbo.algorithms.kmeans;
+
+import com.google.common.io.Resources;
+import weka.clusterers.SimpleKMeans;
+import weka.core.Instances;
+import weka.core.converters.ArffLoader;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+/**
+ * @date: 27/11/2017 6:58 PM
+ * @author: qinjiangbo@github.io
+ * @description:
+ *      使用KMeans算法将Iris数据集进行聚类
+ */
+public class KmeansCluster {
+
+    /**
+     * 训练数据集文件地址
+     */
+    private static final String TRAINING_DATASET_FILENAME = "kmeans/iris.arff";
+
+    public static void main(String[] args) {
+        try {
+            // 进行聚类
+            process();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 这个方法主要是用来加载数据集的
+     * @param fileName 文件路径
+     * @return
+     */
+    public static Instances loadDataSet(String fileName) {
+        Instances instances = null;
+        URL url = Resources.getResource(fileName);
+        File file = new File(url.getPath());
+        ArffLoader loader = new ArffLoader();
+        try {
+            loader.setFile(file);
+            instances = loader.getDataSet();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return instances;
+    }
+
+    /**
+     * 对数据集进行操作
+     * @throws Exception
+     */
+    public static void process() throws Exception {
+        Instances instances = loadDataSet(TRAINING_DATASET_FILENAME);
+        // 初始化聚类器
+        SimpleKMeans kMeans = new SimpleKMeans();
+        // 设置聚类要得到的簇数
+        kMeans.setNumClusters(3);
+        // 开始进行聚类
+        kMeans.buildClusterer(instances);
+        // 打印聚类结果
+        System.out.println(kMeans.preserveInstancesOrderTipText());
+        System.out.println(kMeans.toString());
+    }
+
+}
