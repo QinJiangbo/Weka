33736030b7f09507a034dc33808508c8e5e425bb
Qin Jiangbo <QinJiangbo@users.noreply.github.com>
2017-11-28 10:52:08 +0800

    添加决策树分析的例子，使用乳腺癌数据集

---
+++ src/main/java/com/qinjiangbo/algorithms/decisiontree/J48DecisionTree.java
@@ -0,0 +1,14 @@
+package com.qinjiangbo.algorithms.decisiontree;
+
+/**
+ * @date: 28/11/2017 10:33 AM
+ * @author: qinjiangbo@github.io
+ * @description:
+ *      使用J48决策树对乳腺癌数据集进行训练和预测
+ */
+public class J48DecisionTree {
+
+    public static void main(String[] args) {
+
+    }
+}
