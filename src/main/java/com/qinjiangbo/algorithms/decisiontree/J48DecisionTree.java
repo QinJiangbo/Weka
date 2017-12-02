a2d4a8d0f3ccc530d236e6acb3941eba722e4579
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2017-12-02 16:42:08 +0800

    重构J48决策树的代码结构

--- src/main/java/com/qinjiangbo/algorithms/decisiontree/J48DecisionTree.java
+++ src/main/java/com/qinjiangbo/algorithms/decisiontree/J48DecisionTree.java
@@ -1,5 +1,6 @@
 package com.qinjiangbo.algorithms.decisiontree;
 
+import com.google.common.collect.Lists;
 import com.google.common.io.Resources;
 import weka.classifiers.Classifier;
 import weka.classifiers.Evaluation;
@@ -12,6 +13,7 @@ import weka.filters.unsupervised.attribute.Remove;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 import java.util.Random;
 
 /**
@@ -30,36 +32,13 @@ public class J48DecisionTree {
     public static void main(String[] args) {
         try {
             Instances instances = loadDataSet(TRAINING_DATASET_FILENAME);
-            Classifier j48 = generateClassifier();
+            // 去除第一列[index=0]
+            instances = filterAttributes(instances);
             // 8,10,10,8,7,10,9,7,1
-            Attribute attr01 = instances.attribute("clump_thickness");
-            Attribute attr02 = instances.attribute("uniformity_of_cell_size");
-            Attribute attr03 = instances.attribute("uniformity_of_cell_shape");
-            Attribute attr04 = instances.attribute("marginal_adhesion");
-            Attribute attr05 = instances.attribute("single_epithelial_cell_size");
-            Attribute attr06 = instances.attribute("bare_nuclei");
-            Attribute attr07 = instances.attribute("bland_chromatin");
-            Attribute attr08 = instances.attribute("normal_nucleoli");
-            Attribute attr09 = instances.attribute("mitoses");
-
-            // 创建Instance
-            Instance instance = new DenseInstance(instances.numAttributes());
-            instance.setValue(attr01, 8);
-            instance.setValue(attr02, 10);
-            instance.setValue(attr03, 10);
-            instance.setValue(attr04, 8);
-            instance.setValue(attr05, 7);
-            instance.setValue(attr06, 10);
-            instance.setValue(attr07, 9);
-            instance.setValue(attr08, 7);
-            instance.setValue(attr09, 1);
-            instance.setDataset(instances);
-            double[] distribution = j48.distributionForInstance(instance);
-
-            for (double dist : distribution) {
-                System.out.println(dist);
-            }
-
+            List<Integer> data = Lists.newArrayList(4,1,1,1,2,1,2,1,1);
+            // 进行预测
+            String classOfData = predict(data, instances);
+            System.out.println("class of data is: " + classOfData);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -111,10 +90,8 @@ public class J48DecisionTree {
      */
     public static Classifier generateClassifier() throws Exception{
         Instances instances = loadDataSet(TRAINING_DATASET_FILENAME);
-        // 去掉第一个属性
-        int[] indices = new int[]{0};
         // 过滤后的数据集
-        instances = filterAttributes(instances, indices);
+        instances = filterAttributes(instances);
         // 初始化分类器
         Classifier j48 = new J48();
         // 训练该数据集
@@ -125,10 +102,11 @@ public class J48DecisionTree {
     /**
      * 过滤掉部分属性
      * @param instances 待过滤数据集
-     * @param indices   需要移除的属性下标，下标从0开始
      * @return
      */
-    public static Instances filterAttributes(Instances instances, int[] indices) {
+    public static Instances filterAttributes(Instances instances) {
+        // 去掉第一个属性
+        int[] indices = new int[]{0};
         // 子数据集
         Instances subset = null;
         // 属性移除过滤器
@@ -143,5 +121,24 @@ public class J48DecisionTree {
         }
         subset.setClassIndex(subset.numAttributes()-1);
         return subset;
+    }
+
+    /**
+     * 打印出当前数据最可能所属的类别
+     * @return
+     */
+    public static String predict(List<Integer> data, Instances trainingSet) throws Exception{
+        Classifier j48 = generateClassifier();
+        // 创建Instance
+        Instance instance = new DenseInstance(trainingSet.numAttributes());
+        // 分别添加待预测特征值
+        for (int i = 0; i < data.size(); i++) {
+            instance.setValue(trainingSet.attribute(i), data.get(i));
+        }
+        // 需要能访问数据集
+        instance.setDataset(trainingSet);
+        // 得出最可能所属类别
+        int index = (int)j48.classifyInstance(instance);
+        return trainingSet.classAttribute().value(index);
     }
 }
